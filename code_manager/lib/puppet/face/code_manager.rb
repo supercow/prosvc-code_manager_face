0c610f3f823c63b7db69aaee736c6c64d28300ec
Anderson Mills <anderson@puppetlabs.com>
2015-11-13 14:57:43 -0800

    flushing out the framework

--- code_manager/lib/puppet/face/code_manager.rb
+++ code_manager/lib/puppet/face/code_manager.rb
@@ -1,3 +1,8 @@
+require 'puppet'
+require 'puppet/face'
+require 'tzinfo'
+require 'json'
+
 Puppet::Face.define(:code_manager, '0.0.1') do
   description <<-DESCRIPTION
 The code_manager face is used to interact with the code-manager service.
@@ -7,9 +12,19 @@ DESCRIPTION
   author "puppetlabs"
   notes "There is a lot to say about code-manager."
   # ...
+
+  action :startall do
+    summary "Start a deploy of all environments"
+    when_invoked do |options|
+      "code-manager startall"
+    end
+  end
+
   action :start do
-    summary "Start a deploy"
+    summary "Start a deploy of one environment"
     arguments "<environment>"
+
+    post_data = {}
 
     option '--all' do
       summary "Start a deploy of all environments."
@@ -21,16 +36,25 @@ DESCRIPTION
       default_to { false }
     end
 
-    when_invoked do |options|
+    when_invoked do |environment, options|
       #[...]
       if options[:all]
         puts "invoked with all"
+        post_data [:all] = true
       end
       if options[:wait]
         puts "invoked with wait"
+        post_data [:wait] = true
+      end
+      if environment
+        puts "invoked with enivronment #{environment}"
+        post_data [:environments] = [ environment ]
       end
       token = File.read( File.join(Dir.home, '.puppetlabs', 'token'))
-      "code-manager start #{options} and token = #{token}"
+      tz = TZInfo::Timezone.get('America/New_York')
+      url = "http://graynoise.konfuzo.net/nobody.txt"
+      cl_output = `curl -sS #{url}`
+      "code-manager start #post_data = #{JSON.generate post_data} \n and cl_output = #{cl_output} \n and now = #{tz.now} \n and token = #{token}"
     end
   end
 end
