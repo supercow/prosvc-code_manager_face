6072c5a25bba13e272ab09566a47efbbb1e41695
Anderson Mills <anderson@puppetlabs.com>
2015-11-17 17:36:26 -0800

    Added server discovery from puppet and cleaned up code.

--- lib/puppet/face/code_manager.rb
+++ lib/puppet/face/code_manager.rb
@@ -5,7 +5,7 @@ require 'puppet/network/http/connection'
 require 'net/http'
 require 'uri'
 
-Puppet::Face.define(:code_manager, '0.0.1') do
+Puppet::Face.define(:code_manager, '0.1.0') do
   description <<-DESCRIPTION
 The code_manager face is used to interact with the code-manager service.
 DESCRIPTION
@@ -13,21 +13,20 @@ DESCRIPTION
   copyright "Puppet Labs", 2015
   author "puppetlabs"
   notes "There is a lot to say about code-manager."
-  # ...
 
   action :startall do
     summary "Start a deploy of all environments."
 
-    post_data = {:all => true}
+    post_body = {:all => true}
 
     option '-w', '--wait' do
-      summary "Wait for the code-manager service to return."
+      summary "Wait for the code-manager service to return"
       default_to { false }
     end
 
     option '-s SERVER', '--cmserver SERVER' do
       summary "Code manager server name"
-      default_to {  }
+      default_to { nil }
     end
 
     option '-p PORT', '--cmport PORT' do
@@ -35,13 +34,13 @@ DESCRIPTION
       default_to { nil }
     end
 
+    option '-t TOKENFILE', '--tokenfile TOKENFILE' do
+      summary "File containing RBAC authorization token"
+      default_to { nil }
+    end
 
     when_invoked do |options|
-      #[...]
-      if options[:wait]
-        post_data [:wait] = true
-      end
-      deploy_call = DeployCall.new(post_data)
+      deploy_call = DeployCall.new(post_body, options)
       deploy_call.result
     end
   end
@@ -50,65 +49,66 @@ DESCRIPTION
     summary "Start a deploy of one environment"
     arguments "<environment>"
 
-    post_data = {}
+    post_body = {}
 
-    option '--wait' do
+    option '-w', '--wait' do
       summary "Wait for the code-manager service to return."
       default_to { false }
     end
 
-    when_invoked do |environment, options|
-      #[...]
-      if options[:wait]
-        puts "invoked with wait"
-        post_data [:wait] = true
-      end
-      if environment
-        puts "invoked with enivronment #{environment}"
-        post_data [:environments] = [ environment ]
-      end
-      deploy_call = DeployCall.new(post_data)
-      deploy_call.result
+    option '-s SERVER', '--cmserver SERVER' do
+      summary "Code manager server name"
+      default_to { nil }
+    end
 
+    option '-p PORT', '--cmport PORT' do
+      summary "Code manager port on server"
+      default_to { nil }
+    end
+
+    option '-t TOKENFILE', '--tokenfile TOKENFILE' do
+      summary "File containing RBAC authorization token"
+      default_to { nil }
+    end
+
+    when_invoked do |environment, options|
+      post_body[:environments] = [ environment ]
+      deploy_call = DeployCall.new(post_body, options)
+      deploy_call.result
     end
   end
 end
 
 class DeployCall
-  def initialize(post_data, server = nil, port = nil)
+  DEFAULT_CODE_MANAGER_PORT = 8170
+  CODE_MANAGER_PATH = "code-manager/v1/deploys"
+
+  def initialize(post_body, options)
+    @post_body = post_body
+    if options[:wait]
+      @post_body[:wait] = true
+    end
+
+    token_file = options[:tokenfile] || File.join(Dir.home, '.puppetlabs', 'token')
+
+    if File.file?(token_file)
+      token = File.read(token_file).gsub('\n','')
+    else
+      raise "Token file does not exist."
+    end
+
     Puppet.settings.preferred_run_mode = "master"
-    @post_data = post_data
-    @token = File.read( File.join(Dir.home, '.puppetlabs', 'token')).chop
-    @code_manager_host = 'localhost'
-    @code_manager_port = 8170
-    @code_manager_path = "code-manager/v1/deploys"
-    #@code_manager_all = "http://#{@code_manager_host}:#{@code_manager_port}/#{@code_manager_path}"
-    @code_manager_all = "http://#{@code_manager_host}:#{@code_manager_port}/#{@code_manager_path}?token=#{@token}"
-    #@code_manager_host = 'graynoise.konfuzo.net'
-    #@code_manager_port = 80
-    #@code_manager_path = 'nobody.txt'
-    require 'pry'; binding.pry
-    @uri = URI(@code_manager_all)
-    @http = Net::HTTP.new(@uri.host, @uri.port)
-    @request = Net::HTTP::Post.new(@uri.request_uri, {'Content-Type' =>'application/json'})
-    @request.body = @post_data.to_json
-    #@request.body = JSON.generate(@post_data)
-    #@request.body = '{"environments":["production"]}'
-    @response = @http.request(@request)
-    #@response = Net::HTTP.post_form(@uri, 'token' => @token, 'body' => JSON.generate(@post_data))
-    #@uri.query = URI.encode_www_form(
-    #@http = Net::HTTP.new(@uri.host, @uri.port)
-    #@request = Net::HTTP::Post.new(@uri.path, 'token' => @token)
-    #@request["token"] = @token
-    #@response = @http.post(@uri.path, JSON.generate(@post_data))
+    code_manager_host = options[:cmserver] || Puppet[:ca_server]
+    code_manager_port = options[:cmport] || DEFAULT_CODE_MANAGER_PORT
+    @code_manager_all = "http://#{code_manager_host}:#{code_manager_port}/#{CODE_MANAGER_PATH}?token=#{token}"
   end
 
   def result
-      "\n code-manager start #post_data = #{JSON.generate @post_data} \n"\
-        " and code_manager_all = #{@code_manager_all} \n"\
-        " and uri.path = #{@uri.path} \n"\
-        " and response = #{@response.body} \n"\
-        " and Puppet[:ca_server] = #{Puppet[:ca_server]}"
-        #" and token = #{@token}"
+    uri = URI(@code_manager_all)
+    http = Net::HTTP.new(uri.host, uri.port)
+    request = Net::HTTP::Post.new(uri.request_uri, {'Content-Type' =>'application/json'})
+    request.body = @post_body.to_json
+    response = http.request(request)
+    response.body
   end
 end
