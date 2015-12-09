c0df1aef235bbc2ecabbb78fe824af30ea6eaacf
Alex Dreyer <alex@puppetlabs.com>
2015-12-09 09:11:44 -0800

    Attempt to add puppet ca or user supplied ca to trust store.

--- lib/puppet/face/code_manager.rb
+++ lib/puppet/face/code_manager.rb
@@ -1,13 +1,16 @@
 require 'puppet'
 require 'puppet/face'
 require 'json'
-require 'puppet/network/http/connection'
 require 'net/http'
+require 'openssl'
+require 'puppet/network/http'
+require 'puppet/network/http_pool'
+require 'puppet/network/http/nocache_pool'
 require 'uri'
 
 Puppet::Face.define(:code_manager, '0.1.0') do
   description <<-DESCRIPTION
-The code_manager face is used to interact with the code-manager service to 
+The code_manager face is used to interact with the code-manager service to
 deploy environments.
 DESCRIPTION
   summary "Used to work with the code-manager service to deploy environments"
@@ -45,9 +48,14 @@ DESCRIPTION
       default_to { nil }
     end
 
+    option '--ca_cert CA_CERT' do
+      summary 'path to a ca certificate to add to default trust store'
+      default_to { "/etc/puppetlabs/puppet/ssl/certs/ca.pem" }
+    end
+
     when_invoked do |options|
       deploy_call = DeployCall.new(post_body, options)
-      deploy_call.result(options[:insecure])
+      deploy_call.result()
     end
   end
 
@@ -82,10 +90,15 @@ DESCRIPTION
       default_to { nil }
     end
 
+    option '--ca_cert CA_CERT' do
+      summary 'path to a ca certificate to add to default trust store'
+      default_to { "/etc/puppetlabs/puppet/ssl/certs/ca.pem" }
+    end
+
     when_invoked do |environment, options|
       post_body["environments"] = [ environment ]
       deploy_call = DeployCall.new(post_body, options)
-      deploy_call.result(options[:insecure])
+      deploy_call.result()
     end
   end
 end
@@ -109,21 +122,54 @@ class DeployCall
       raise "Token file does not exist or is not readable."
     end
 
-    Puppet.settings.preferred_run_mode = "master"
-    code_manager_host = options[:cmserver] || Puppet[:ca_server]
+    if options[:insecure]
+      @cert_store = nil
+    else
+      @cert_store = get_store(options[:ca_cert])
+    end
+
+    Puppet.settings.preferred_run_mode = "agent"
+    code_manager_host = options[:cmserver] || Puppet[:server]
     code_manager_port = options[:cmport] || DEFAULT_CODE_MANAGER_PORT
+
     @code_manager_all = "https://#{code_manager_host}:#{code_manager_port}/#{CODE_MANAGER_PATH}?token=#{token}"
   end
 
-  def result(insecure = false)
+  def get_store(ca_cert)
+    cert_store = OpenSSL::X509::Store.new
+    cert_store.set_default_paths
+    if File.file?(ca_cert)
+      cert_store.add_file(ca_cert)
+    else
+      Puppet.warning "Could not load ca_certificate #{ca_cert} using default store"
+    end
+    cert_store
+  end
+
+  def result()
     uri = URI(@code_manager_all)
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true if uri.scheme == 'https'
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE if insecure
+    if uri.scheme == 'https'
+      http.use_ssl = true
+      if @cert_store
+        http.cert_store = @cert_store
+      else
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      end
+    end
     request = Net::HTTP::Post.new(uri.request_uri, {'Content-Type' =>'application/json'})
     request.body = @post_body.to_json
-    response = http.request(request)
-    JSON.pretty_generate(JSON.parse(response.body))
-  end
+    begin
+      response = http.request(request)
+    rescue Exception => e
+      raise "Request to #{@code_manager_all} failed: #{e}"
+    end
 
+    begin
+      JSON.pretty_generate(JSON.parse(response.body))
+    rescue Exception
+      raise "Could not parse response body: #{response.body}"
+    end
+  end
 end
